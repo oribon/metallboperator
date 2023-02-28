b502974663e8247d63d8f3696b2c47cca502c530
liornoy <lnoy@redhat.com>
2023-02-28 15:39:07 +0200

    Read the memberlist secret from a file
    This commit backports the fix for the memberlist secret bug
    see more here: https://github.com/openshift/metallb/pull/107
    
    Signed-off-by: liornoy <lnoy@redhat.com>

--- hack/common.sh
+++ hack/common.sh
@@ -17,15 +17,15 @@ export PATH=$PATH:$GOPATH/bin
 
 mkdir -p _cache
 
-export METALLB_COMMIT_ID="fb96318bb4d01495098c3ee122f896b67cf9b177"
+export METALLB_COMMIT_ID="a917ee063c03ab4f465d142f5fb242d390c842b1"
 export METALLB_PATH=_cache/metallb
 
 export METALLB_SC_FILE=$(dirname "$0")/securityContext.yaml
 
 function fetch_metallb() {
     if [[ ! -d "$METALLB_PATH" ]]; then
-        curl -L https://github.com/metallb/metallb/tarball/"$METALLB_COMMIT_ID" | tar zx -C _cache
+        curl -L https://github.com/openshift/metallb/tarball/"$METALLB_COMMIT_ID" | tar zx -C _cache
         rm -rf "$METALLB_PATH"
-        mv _cache/metallb-metallb-* "$METALLB_PATH"
+        mv _cache/openshift-metallb-* "$METALLB_PATH"
     fi
 }
