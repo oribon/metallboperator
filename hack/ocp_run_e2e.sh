4fb0a854634f251d5a03f44753e633a2e25c1bb3
Ori Braunshtein <obraunsh@redhat.com>
2025-01-27 17:12:10 +0200

    OpenShift only: pass frr-k8s external namespace to e2e
    Signed-off-by: Ori Braunshtein <obraunsh@redhat.com>

--- hack/ocp_run_e2e.sh
+++ hack/ocp_run_e2e.sh
@@ -4,6 +4,7 @@ export PATH=$PATH:$(pwd)/_cache
 export OO_INSTALL_NAMESPACE=openshift-metallb-operator
 export USE_LOCAL_RESOURCES=true
 export IS_OPENSHIFT=1
+export FRRK8S_EXTERNAL_NAMESPACE="openshift-frr-k8s"
 
 hack/validate_ocp_bundle.sh
 go test --tags=validationtests -v ./test/e2e/validation -ginkgo.v -junit /logs/artifacts/ -report /logs/artifacts/
