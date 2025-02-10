bbad9313d575b349f90e6dd68b74ab8729b243b1
Ori Braunshtein <obraunsh@redhat.com>
2025-02-10 12:52:03 +0200

    OpenShift only: pass frr-k8s external namespace to e2e
    We pass the namespace so the test can run using CNO, we
    disable it for now until the CI cluster version can be
    bumped to 4.19+.
    
    Signed-off-by: Ori Braunshtein <obraunsh@redhat.com>

--- hack/ocp_run_e2e.sh
+++ hack/ocp_run_e2e.sh
@@ -4,7 +4,8 @@ export PATH=$PATH:$(pwd)/_cache
 export OO_INSTALL_NAMESPACE=openshift-metallb-operator
 export USE_LOCAL_RESOURCES=true
 export IS_OPENSHIFT=1
+export FRRK8S_EXTERNAL_NAMESPACE="openshift-frr-k8s"
 
 hack/validate_ocp_bundle.sh
 go test --tags=validationtests -v ./test/e2e/validation -ginkgo.v -junit /logs/artifacts/ -report /logs/artifacts/
-go test --tags=e2etests -v ./test/e2e/functional -ginkgo.v -ginkgo.skip "frr-k8s" -junit /logs/artifacts/ -report /logs/artifacts/
+go test --tags=e2etests -v ./test/e2e/functional -ginkgo.v -ginkgo.skip "with BGP type" -junit /logs/artifacts/ -report /logs/artifacts/
